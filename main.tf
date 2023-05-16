17bbb65c42773aa0614b44fca380c615a8b13223
ahmedhassan1999 <68762208+ahmedhassan1999@users.noreply.github.com>
2023-05-16 17:10:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 0.7"
+      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 0.7"
 }
 
 provider "aws" {
